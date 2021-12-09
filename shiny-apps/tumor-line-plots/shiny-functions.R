35978a142a5745bdbcd76be346a98161ae671a1e
dspak <daniel.spakowicz@gmail.com>
2021-12-09 11:11:25 -0500

    visualization tweaks

--- shiny-apps/tumor-line-plots/shiny-functions.R
+++ shiny-apps/tumor-line-plots/shiny-functions.R
@@ -7,14 +7,15 @@ plotDesiredGroups <- function(radselected){
   tmp.plot <- tumor.volumes %>%
     filter(treatment.group %in% radselected) %>%
     ggplot(aes(x = time.point, y = mean.volume, group = treatment.group, color = treatment.group)) +
-    geom_line(position = position_dodge(width = .5))+
+    geom_line(position = position_dodge(width = .5), lwd = 1.3)+
     geom_point(position = position_dodge(width = .5))+
     labs(title = "Tumor Volume vs. Time for Mimic", x = "Time point", y = "Mean tumor volume") +
     theme_bw() +
     scale_color_manual(breaks = radselected, values = getPallete(length(radselected)),
                        name = "Treatment group") +
     geom_errorbar(aes(ymin = lwr, ymax = upr),
-                  width = 1,
-                  position = position_dodge(width = .5))
+                  width = 0.3,
+                  position = position_dodge(width = .5),
+                  lwd = 1.3)
   return(tmp.plot)
 }
