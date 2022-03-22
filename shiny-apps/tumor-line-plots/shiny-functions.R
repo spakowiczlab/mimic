fccf4dd1703d04bcbfca8149bbe1d0ae086cbe48
hoyd44 <rhoyd44@gmail.com>
2022-03-22 17:08:00 +0100

    Update to add mimic8 and swap x axis to days in shiny app

--- shiny-apps/tumor-line-plots/shiny-functions.R
+++ shiny-apps/tumor-line-plots/shiny-functions.R
@@ -1,15 +1,15 @@
 
-tumor.volumes <- read.csv("2022-03-03_mean-tumor-volumes.csv", stringsAsFactors = F)
+tumor.volumes <- read.csv("2022-03-22_mean-tumor-volumes.csv", stringsAsFactors = F)
 
 getPallete <- colorRampPalette(brewer.pal(8, "Dark2"))
 
 plotDesiredGroups <- function(radselected){
   tmp.plot <- tumor.volumes %>%
     filter(treatment.group %in% radselected) %>%
-    ggplot(aes(x = time.point, y = mean.volume, group = treatment.group, color = treatment.group)) +
+    ggplot(aes(x = days.from.injection, y = mean.volume, group = treatment.group, color = treatment.group)) +
     geom_line(position = position_dodge(width = .5), lwd = 1.3)+
     geom_point(position = position_dodge(width = .5))+
-    labs(title = "Tumor Volume vs. Time for Mimic", x = "Time point", y = "Mean tumor volume") +
+    labs(title = "Tumor Volume vs. Time for Mimic", x = "Days from Tumor Injection", y = "Mean tumor volume") +
     theme_bw() +
     scale_color_manual(breaks = radselected, values = getPallete(length(radselected)),
                        name = "Treatment group") +
